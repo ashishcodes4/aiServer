d1a629f5d64932b21039ff3969915bace2736aa9
ashichcodes4 <ashishcodes4@gmail.com>
2018-02-13 04:31:16 +0000

    clarifai api loaded withoutany errors,,, getting the console output on both the terminals.. @ashish

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-console.log('connected');
+/* global Clarifai */
+const clarifai = new Clarifai.App({
+    apiKey: 'c9a7740b61cd42139a16a93d5a2da926'
+});
+
+clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+.then(function(response){
+    console.log(response)
+})
+.catch(function(err){
+    console.log(err)
+})
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 
 
 
+
+
+
+
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT;
-
+const Clarifai = require("clarifai");
+const clarifai = new Clarifai.App({
+    apiKey: 'c9a7740b61cd42139a16a93d5a2da926'
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+
 
 // app.get('/', function(req, res){
 //     res.sendFile('index.html');
