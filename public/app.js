57d0253f301f2f048c916f8e0c7cca587601af3e
ashichcodes4 <ashishcodes4@gmail.com>
2018-02-14 02:44:37 +0000

    second day testing with clarifai API, no errors... @ashish

--- public/app.js
+++ public/app.js
@@ -1,12 +1,30 @@
 /* global Clarifai */
-const clarifai = new Clarifai.App({
-    apiKey: 'c9a7740b61cd42139a16a93d5a2da926'
+// /* global FACE_DETECT_MODEL */
+const inputUrl = document.querySelector('#url');
+const sbBtn = document.querySelector('#sb-btn');
+const inputImg = document.querySelector('#inputImage');
+
+
+
+
+sbBtn.addEventListener('click', function(){
+    let link = inputUrl.value;
+    console.log(link);
+    
 });
 
-clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
-.then(function(response){
-    console.log(response)
-})
-.catch(function(err){
-    console.log(err)
-})
+
+
+// const clarifai = new Clarifai.App({
+//     apiKey: 'c9a7740b61cd42139a16a93d5a2da926'
+// });
+
+// clarifai.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg")
+// .then(
+//     function(response) {
+//       console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+//     }
+//   )
+//   .catch(function(err){
+//       console.log(err);
+//   });
